1518c87b2e711b5a22b8c6d2bb91154ea356410c
Realpra1 <realpra1@hotmail.com>
2015-10-08 17:50:59 +0200

    Version
    Set release version and debug false (set back to true later).

--- src/com/blochstech/bitcoincardterminal/Utils/Tags.java
+++ src/com/blochstech/bitcoincardterminal/Utils/Tags.java
@@ -4,7 +4,7 @@ package com.blochstech.bitcoincardterminal.Utils;
 //To filter on it click LogCat, click the plus sign, name your filter and under Tag input the configured tag here - save and use.
 public class Tags {
 	//Set this to false when building to Google Play app store.
-	public static final boolean DEBUG = true;
+	public static final boolean DEBUG = false;
 	
 	public static final String APP_TAG = "BitcoinCardTerminal";
 }
